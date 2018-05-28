730aefe7f2ad697264ef91f7c611c590ecb7ca86
nyugoh <nyugoh@gmail.com>
2018-05-28 20:59:16 +0300

    Chapter 1 and 2

---
+++ data-types/booleans.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println(true && true) // TRUE
+    fmt.Println(false && false) // false
+    fmt.Println(true && false) // FALSE
+    fmt.Println(true || true) // TRUE
+    fmt.Println(false || false) // false
+    fmt.Println(true || false) // TRUE
+    fmt.Println(!true) // FALSE
+    fmt.Println(!false) // TRUE
+}
---
+++ data-types/numbers.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("1 + 1 = ", 1+1)
+  fmt.Println("1.0 + 1.0 = ", 1.0+1.0)
+  fmt.Println("4 * 2 = ", 2 * 4)
+  fmt.Println("4 / 2 = ", 4 / 2)
+  fmt.Println("4 % 2 = ", 4 % 3)
+}
