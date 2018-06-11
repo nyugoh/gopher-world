069984aa2dc83c3cf115641240c12a17c4f467ee
nyugoh <nyugoh@gmail.com>
2018-06-11 13:46:29 +0300

    Added menu and welcome message

--- shopping.go
+++ shopping.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	utils "github.com/nyugoh/go-shopping-cli/utils"
+	"fmt"
+	"strconv"
+)
 
+var (
+	menuOption string
+)
 func main() {
-	fmt.Println("Hello world")
+	utils.WelcomeMessage()
+	utils.MainMenu()
+	fmt.Scan(&menuOption)
+	setOption(menuOption)
+}
+
+func setOption(s string)  {
+	i, _ := strconv.Atoi(s)
+	switch i {
+	case 0:
+		//exit app
+		fmt.Println("Exiting app ...")
+	case 1:
+		//cal
+		fmt.Println("selected 1")
+	default:
+		fmt.Println("No option selected")
+	}
 }
---
+++ utils/utils.go
@@ -0,0 +1,26 @@
+package utils
+
+import "fmt"
+
+func WelcomeMessage() {
+	fmt.Println("\n========= Welcome to the best shopping cart =========")
+	fmt.Println(" We offer Gopher products for awesome people like you ")
+	fmt.Println("                 Happy shopping")
+	fmt.Printf("*****************************************************\n\n\n")
+}
+
+func MainMenu()  {
+	fmt.Println("|------------------ MENU -------------|")
+	fmt.Println("              Main Menu")
+	fmt.Println("Use the options below to navigate")
+	fmt.Println("1. Add item")
+	fmt.Println("2. List items")
+	fmt.Println("3. Select item")
+	fmt.Println("4. Checkout/ Get cart status")
+	fmt.Println("5. Pay")
+	fmt.Println("6. Print receipt")
+	fmt.Println("0. Display menu")
+	fmt.Println("q - quit the application")
+	fmt.Println("|___________________ END ____________|")
+	fmt.Printf("Select ==> ")
+}
